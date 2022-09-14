aac67d02f27d261c104857b0a39c22d976c07222
Mahmoud819 <67577538+Mahmoud819@users.noreply.github.com>
2022-09-13 19:05:18 -0500

    Update Class_Exercise1.py

--- Class_Exercise1.py
+++ Class_Exercise1.py
@@ -1,14 +1,16 @@
-a=print("Enter the two nums: ")
+a=print("Enter the 3 nums: ")
 b=int(input())
 b1=int(input())
-c= b-b1
-c1=b+b1
-c2=b*b1
-c3=b**b1
-c4=b/b1
+b2 = int(input())
+c= b-b1-b2
+c1=b+b1+b2
+c2=b*b1*b2
+c3=b**(b1+b2)
+c4=(b2+b)/b1
 print(c)
 print(c1)
 print(c2)
 print(c3)
 print(c4)
 
+Mahmoud and Tina
